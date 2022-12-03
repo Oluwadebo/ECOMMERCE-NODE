dc796876f9cd560a806a5013f40f6fa7f4ae72f7
Oluwadebo <ogunweoluwadebo@gmail.com>
2022-12-03 19:29:48 +0100

    frontend done

--- forntend/src/component/Addtocart.js
+++ forntend/src/component/Addtocart.js
@@ -1,12 +1,54 @@
 import React from 'react'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import footballboots from "./asset/football_boots_198704.jpg"
+import footballboot8 from "./asset/football_boots_198708.jpg"
 
 const Addtocart = () => {
     return (
         <>
             <Navbar />
-            <div className="container-fluid mt-5 pt-4 mb-4 p-0 m-0">
+            <div className="container-fluid mt-5 pt-1 mb-4 p-0 m-0">
+                <div className="">
+                    <section className="new-product">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-3">
+                                    <div className="product-top">
+                                        <div className="imgBx">
+                                            <img src={footballboots} className="h-100" />
+                                        </div>
+                                        <div className="product-botttom text-center">
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star-half"></i>
+                                            <h3>Men's Soccer Boot</h3>
+                                            <h5>$40.00</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="product-top">
+                                        <div className="imgBx">
+                                            <img src={footballboot8} className="h-100" />
+                                        </div>
+                                        <div className="product-botttom text-center">
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star-half"></i>
+                                            <h3>Men's Soccer Boot</h3>
+                                            <h5>$70.00</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
                 <Footer />
             </div>
         </>
